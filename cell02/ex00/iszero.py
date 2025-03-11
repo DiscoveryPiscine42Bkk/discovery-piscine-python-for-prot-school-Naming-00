bdbff9dfb68cf85b0d1bd5649e4ed171d91160c2
Naming-00 <40334@prot.ac.th>
2025-03-11 09:23:55 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,10 @@
+number = int(input("please enter number : "))
+
+if number < 0 :
+    print("this number is negative. ")
+
+elif number > 0 :
+    print(" this number is positive. ")
+ 
+elif number == 0 :
+    print("this number is both positive and negative. ")
